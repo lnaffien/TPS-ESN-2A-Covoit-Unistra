42e8d44e98157415d857cad4645f95c4fb10efc9
unknown <lucie@naffien.com>
2024-04-16 15:33:41 +0200

    Merging Front and Refractoring : Header anonymous

--- src/View/header_anonymous.php
+++ src/View/header_anonymous.php
@@ -4,24 +4,11 @@
   
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <link rel="stylesheet" type="text/css" href="css/menu.css">
-  <title> Page de connexion </title>
+  <link rel="stylesheet" href="css/connexion.css" />
 </head>
 
-<header class="header">
+  <header class="HeaderNotLogged">
+    <h1 class="CovoitUnistra">COVOIT’ UNISTRA</h1>
+  </header>
 
-<div class="flex justify-between align-center">
-
-    <img src="images/maison.png" class="home-logo">
-
-    <div>
-        <h1 class="logo-unistra1"> COVOIT' </h1>
-        <h1 class="logo-unistra2"> UNISTRA </h1>
-    </div>
-
-</div>
-
-</header>
 </html>
