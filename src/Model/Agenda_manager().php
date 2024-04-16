70e0ad817146f23d14521cb1201902dcd38d1ad0
unknown <lucie@naffien.com>
2024-04-16 09:26:55 +0200

    Deleting unwanted files

--- src/Model/Agenda_manager().php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * TODO Auto-generated comment.
- */
-class Agenda_manager() {
-}
--- src/Model/User_login_manager().php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * TODO Auto-generated comment.
- */
-class User_login_manager() extends User_connection_manager() {
-}
