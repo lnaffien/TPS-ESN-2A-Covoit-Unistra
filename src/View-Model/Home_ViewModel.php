acf1383a79c23deabdf9c76e565495bbb69a3e62
unknown <lucie@naffien.com>
2024-04-18 09:57:42 +0200

    Home_ViewModel update

--- src/View-Model/Home_ViewModel.php
+++ src/View-Model/Home_ViewModel.php
@@ -1,7 +1,27 @@
 <?php
-/**
- * TODO Auto-generated comment.
- */
+
+if(isset($_POST['home_page_click']))
+{
+    $next_page_instruction = 'homepage';
+
+    switch($_POST['home_page_click'])
+    {
+        case 'reload_calendar_data' :
+            $next_page_instruction = 'homepage';
+            break;
+
+    }
+    print("<form id='form' name='action_next_page' action='../../index.php' method='POST'>
+                <input type='hidden' name='action' value='$next_page_instruction'>
+            </form>
+            <script type='text/javascript'>
+                document.action_next_page.submit();
+            </script>
+        ");
+
+}
+
+
 class Home_ViewModel
 {
     public static function execute()
@@ -9,12 +29,13 @@ class Home_ViewModel
         require_once('src/Model/User.php');
         require_once('src/Model/Agenda_manager.php');
 
-        include('src/View/header_connected.php');
+       // include('src/View/header_connected.php');
 
         // Load agenda for 7 days starting from the current date
         // TODO : Timezone
         $currentDate = (new \DateTime((string)date('Y-m-d H:i:s')))->setTimezone(new \DateTimeZone('Europe/Paris'));
-        $_SESSION['dateSelected'] = $currentDate->format('Y-m-d');
+        $_SESSION['dateSelectedStart'] = $currentDate;
+        $_SESSION['dateSelectedEnd'] = (clone $currentDate)->modify("7 days");
         
         require_once('src/View/home_page.php');
         $calendarFull = Agenda_manager::get_full_agenda($_SESSION['user']->__get('nagenda'));
