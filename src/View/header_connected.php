15fda1dde17cbd5345c4995e89e64fe37c267909
unknown <lucie@naffien.com>
2024-04-18 17:47:19 +0200

    Flex css

--- src/View/header_connected.php
+++ src/View/header_connected.php
@@ -4,30 +4,28 @@
 <head>  
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link rel="stylesheet" href="css/editdata.css" />
+  <link rel="stylesheet" href="css/style.css" />
 </head>
 
-<header class="HeaderLoggedIn">
+<header>
 
-    <h2 class="PrenomNom"><?php print_r($_SESSION['user']->__get('nom') . ' ' . $_SESSION['user']->__get('prenom'));?></h2>  
-
-
-    <form id="form" action='index.php' method="POST" class="UserButton">
-        <input type="hidden" name="action" value="user_profile">
-        <input type="image" src="images/profileuser.svg" alt="User profile Icon">
-    </form>
-    
-    <form id="form" action='index.php' method="POST">
-        <input type="hidden" name="action" value="friend">
-        <input type='image' class="FriendsButton" src="images/friends.svg" alt="Friends Icon"/>
-    </form>
-
-    <form id="form" action='index.php' method="POST" class="CovoitUnistra">
+    <form action='index.php' method="POST" class="CovoitButton">
         <input type="hidden" name="action" value="homepage">
-        <h2 class="CovoitButton">
+        <h2>
             <input type='submit' value="COVOIT’ UNISTRA" class="CovoitButton">
         </h2>
+    </form>
 
+    <h2><?php print_r($_SESSION['user']->__get('nom') . ' ' . $_SESSION['user']->__get('prenom'));?></h2>  
+
+    <form action='index.php' method="POST">
+        <input type="hidden" name="action" value="friend">
+        <input type='image' src="images/friends.svg" alt="Friends Icon"/>
+    </form>
+
+    <form action='index.php' method="POST">
+        <input type="hidden" name="action" value="user_profile">
+        <input type="image" src="images/profileuser.svg" alt="User profile Icon">
     </form>
 
 </header>
